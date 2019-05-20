89be577a182112c1be6f06169ac7ffafc9aeb56e
github username <github email address>
2019-05-20 14:47:44 -0700

    unfollow_functionality_complete

--- app/controllers/userfollowerrelationships_controller.rb
+++ app/controllers/userfollowerrelationships_controller.rb
@@ -2,6 +2,11 @@ class UserfollowerrelationshipsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
+  if Userfollowerrelationship.find_by(relationship_params)
+    @relationship = Userfollowerrelationship.find_by(relationship_params)
+    @relationship.destroy
+    render json: {message: 'follow relationship destroyed'}, status: :accepted
+  else
     @relationship = Userfollowerrelationship.new(relationship_params)
     if @relationship.save
       render json: @relationship, status: :accepted
@@ -9,9 +14,9 @@ class UserfollowerrelationshipsController < ApplicationController
       render json: {errors: 'Failed to follow user'}, status: :unprocessible_entity
     end
   end
+  end
 
   def index
-  
     @all = Userfollowerrelationship.all
     render json: @all, status: :accepted
   end
