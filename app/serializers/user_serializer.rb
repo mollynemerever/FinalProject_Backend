221282968ace2cb3f746c5091b83991b01d33916
github username <github email address>
2019-05-28 09:28:26 -0700

    edit account page functionality complete

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :full_name, :image, :join_year, :industry
+  attributes :id, :email, :full_name, :image, :join_year, :industry
   has_many :userbooks
   #has_many :books, through: :userbooks
 
